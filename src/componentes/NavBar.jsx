2f3957bc15cc7fdba583523d978e282f25085729
Toto <totocabj06@gmail.com>
2024-03-12 19:04:20 -0300

    primer comit

---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NavBar = () => {
+  return (
+    <>
+      <p href="#">Olympian Tech Hub</p>
+      <div className='Categorias'>
+        <a href="#">Dispositivos Móviles</a>
+        <a href="#">Electrodomésticos y Tecnología para el Hogar</a>
+        <a href="#">Computadoras y Accesorios</a>
+      </div>
+    </>
+  );
+}
